3736b83a9e47671edcef288862b9cd70a37b21be
davidbouchard <david.bouchard@ryerson.ca>
2022-06-09 15:17:00 -0400

    Update 16n.cpp
    Connecting to the midi device no longer reset fader positions.
    Fixed variable names to be more consistent

--- src/16n.cpp
+++ src/16n.cpp
@@ -19,14 +19,14 @@ struct VCVLightSliderTall : LightSlider<VCVSliderTall, VCVSliderLight<TLightBase
 
 struct _16n : Module {
 	enum ParamId {
-		ENUMS(FADER_PARAM, 16),
+		ENUMS(FADER_PARAMS, 16),
 		PARAMS_LEN
 	};
 	enum InputId {
 		INPUTS_LEN
 	};
 	enum OutputId {
-		ENUMS(CV_OUTPUT, 16),
+		ENUMS(CV_OUTPUTS, 16),
 		OUTPUTS_LEN
 	};
 	enum LightId {
@@ -43,20 +43,20 @@ struct _16n : Module {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		
 		for (int i=0; i < 16; i++) {
-			configParam(FADER_PARAM + i, 0.f, 1.f, 0.f, string::f("Fader %d", i+1), " v", 0, 10);
-			configOutput(CV_OUTPUT + i, string::f("CV %d", i+1));
+			configParam(FADER_PARAMS + i, 0.f, 1.f, 0.f, string::f("Fader %d", i+1), " v", 0, 10);
+			configOutput(CV_OUTPUTS + i, string::f("CV %d", i+1));
 
 			lights[FADER_LIGHTS + i].setBrightness(1);
 			
 			// TODO: make this configurable? maybe?
 			// default 16n CC values are 32-47 
-			faderCCs[i] = 32+i;
+			faderCCs[i] = 32+i;			
 		}
-		onReset();
+		//onReset();
 	}
 
 	void onReset() override { 
-		midiInput.reset();
+		midiInput.reset();		
 	}
 
 	void process(const ProcessArgs& args) override {
@@ -68,19 +68,22 @@ struct _16n : Module {
 
 		if (midiInput.getDeviceId() >= 0) { 
 			// Midi device connected
-			for (int id = 0; id < 16; id++) {				
-				int8_t cc = faderCCs[id];				
+			for (int i = 0; i < 16; i++) {				
+				int8_t cc = faderCCs[i];				
 				int16_t cellValue = int16_t(ccValues[cc]) * 128;
 				float value = float(cellValue) / (128 * 127);
 				value = clamp(value, 0.f, 1.f);
-				params[FADER_PARAM + id].setValue(value);
-				outputs[CV_OUTPUT + id].setVoltage(value * 10.f);			
+				params[FADER_PARAMS + i].setValue(value);
+				outputs[CV_OUTPUTS + i].setVoltage(value * 10.f);			
 			}
 		}
 		else {
 			// use the slider manual control if no 16n faderbank is connected
-			for (int id = 0; id < 16; id++) {	
-				outputs[CV_OUTPUT + id].setVoltage(params[FADER_PARAM + id].getValue() * 10.f);				
+			for (int i = 0; i < 16; i++) {	
+				outputs[CV_OUTPUTS + i].setVoltage(params[FADER_PARAMS + i].getValue() * 10.f);				
+				// also update the ccValues, so that current positions are maintained when
+				// we connect to the midi device. 
+				ccValues[faderCCs[i]] = int(params[FADER_PARAMS + i].getValue() * 127);				
 			}
 		}
 	}
@@ -119,39 +122,39 @@ struct _16nWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createLightParamCentered<VCVLightSliderTall<YellowLight>>(mm2px(Vec(9.18, 75.001)), module, _16n::FADER_PARAM + 0, _16n::FADER_LIGHTS + 0));
-		addParam(createLightParamCentered<VCVLightSliderTall<YellowLight>>(mm2px(Vec(22.18, 75.001)), module, _16n::FADER_PARAM + 1, _16n::FADER_LIGHTS + 1));
-		addParam(createLightParamCentered<VCVLightSliderTall<YellowLight>>(mm2px(Vec(35.18, 75.001)), module, _16n::FADER_PARAM + 2, _16n::FADER_LIGHTS + 2));
-		addParam(createLightParamCentered<VCVLightSliderTall<YellowLight>>(mm2px(Vec(48.18, 75.001)), module, _16n::FADER_PARAM + 3, _16n::FADER_LIGHTS + 3));
-		addParam(createLightParamCentered<VCVLightSliderTall<BlueLight>>(mm2px(Vec(61.18, 75.001)), module, _16n::FADER_PARAM + 4, _16n::FADER_LIGHTS + 4));
-		addParam(createLightParamCentered<VCVLightSliderTall<BlueLight>>(mm2px(Vec(74.18, 75.001)), module, _16n::FADER_PARAM + 5, _16n::FADER_LIGHTS + 5));
-		addParam(createLightParamCentered<VCVLightSliderTall<BlueLight>>(mm2px(Vec(87.18, 75.001)), module, _16n::FADER_PARAM + 6, _16n::FADER_LIGHTS + 6));
-		addParam(createLightParamCentered<VCVLightSliderTall<BlueLight>>(mm2px(Vec(100.18, 75.001)), module, _16n::FADER_PARAM + 7, _16n::FADER_LIGHTS + 7));
-		addParam(createLightParamCentered<VCVLightSliderTall<GreenLight>>(mm2px(Vec(113.18, 75.001)), module, _16n::FADER_PARAM + 8, _16n::FADER_LIGHTS + 8));
-		addParam(createLightParamCentered<VCVLightSliderTall<GreenLight>>(mm2px(Vec(126.18, 75.001)), module, _16n::FADER_PARAM + 9, _16n::FADER_LIGHTS + 9));
-		addParam(createLightParamCentered<VCVLightSliderTall<GreenLight>>(mm2px(Vec(139.18, 75.001)), module, _16n::FADER_PARAM + 10, _16n::FADER_LIGHTS + 10));
-		addParam(createLightParamCentered<VCVLightSliderTall<GreenLight>>(mm2px(Vec(152.18, 75.001)), module, _16n::FADER_PARAM + 11, _16n::FADER_LIGHTS + 11));
-		addParam(createLightParamCentered<VCVLightSliderTall<WhiteLight>>(mm2px(Vec(165.18, 75.001)), module, _16n::FADER_PARAM + 12, _16n::FADER_LIGHTS + 12));
-		addParam(createLightParamCentered<VCVLightSliderTall<WhiteLight>>(mm2px(Vec(178.18, 75.001)), module, _16n::FADER_PARAM + 13, _16n::FADER_LIGHTS + 13));
-		addParam(createLightParamCentered<VCVLightSliderTall<WhiteLight>>(mm2px(Vec(191.18, 75.001)), module, _16n::FADER_PARAM + 14, _16n::FADER_LIGHTS + 14));
-		addParam(createLightParamCentered<VCVLightSliderTall<WhiteLight>>(mm2px(Vec(204.18, 75.001)), module, _16n::FADER_PARAM + 15, _16n::FADER_LIGHTS + 15));
+		addParam(createLightParamCentered<VCVLightSliderTall<YellowLight>>(mm2px(Vec(9.18, 75.001)), module, _16n::FADER_PARAMS + 0, _16n::FADER_LIGHTS + 0));
+		addParam(createLightParamCentered<VCVLightSliderTall<YellowLight>>(mm2px(Vec(22.18, 75.001)), module, _16n::FADER_PARAMS + 1, _16n::FADER_LIGHTS + 1));
+		addParam(createLightParamCentered<VCVLightSliderTall<YellowLight>>(mm2px(Vec(35.18, 75.001)), module, _16n::FADER_PARAMS + 2, _16n::FADER_LIGHTS + 2));
+		addParam(createLightParamCentered<VCVLightSliderTall<YellowLight>>(mm2px(Vec(48.18, 75.001)), module, _16n::FADER_PARAMS + 3, _16n::FADER_LIGHTS + 3));
+		addParam(createLightParamCentered<VCVLightSliderTall<BlueLight>>(mm2px(Vec(61.18, 75.001)), module, _16n::FADER_PARAMS + 4, _16n::FADER_LIGHTS + 4));
+		addParam(createLightParamCentered<VCVLightSliderTall<BlueLight>>(mm2px(Vec(74.18, 75.001)), module, _16n::FADER_PARAMS + 5, _16n::FADER_LIGHTS + 5));
+		addParam(createLightParamCentered<VCVLightSliderTall<BlueLight>>(mm2px(Vec(87.18, 75.001)), module, _16n::FADER_PARAMS + 6, _16n::FADER_LIGHTS + 6));
+		addParam(createLightParamCentered<VCVLightSliderTall<BlueLight>>(mm2px(Vec(100.18, 75.001)), module, _16n::FADER_PARAMS + 7, _16n::FADER_LIGHTS + 7));
+		addParam(createLightParamCentered<VCVLightSliderTall<GreenLight>>(mm2px(Vec(113.18, 75.001)), module, _16n::FADER_PARAMS + 8, _16n::FADER_LIGHTS + 8));
+		addParam(createLightParamCentered<VCVLightSliderTall<GreenLight>>(mm2px(Vec(126.18, 75.001)), module, _16n::FADER_PARAMS + 9, _16n::FADER_LIGHTS + 9));
+		addParam(createLightParamCentered<VCVLightSliderTall<GreenLight>>(mm2px(Vec(139.18, 75.001)), module, _16n::FADER_PARAMS + 10, _16n::FADER_LIGHTS + 10));
+		addParam(createLightParamCentered<VCVLightSliderTall<GreenLight>>(mm2px(Vec(152.18, 75.001)), module, _16n::FADER_PARAMS + 11, _16n::FADER_LIGHTS + 11));
+		addParam(createLightParamCentered<VCVLightSliderTall<WhiteLight>>(mm2px(Vec(165.18, 75.001)), module, _16n::FADER_PARAMS + 12, _16n::FADER_LIGHTS + 12));
+		addParam(createLightParamCentered<VCVLightSliderTall<WhiteLight>>(mm2px(Vec(178.18, 75.001)), module, _16n::FADER_PARAMS + 13, _16n::FADER_LIGHTS + 13));
+		addParam(createLightParamCentered<VCVLightSliderTall<WhiteLight>>(mm2px(Vec(191.18, 75.001)), module, _16n::FADER_PARAMS + 14, _16n::FADER_LIGHTS + 14));
+		addParam(createLightParamCentered<VCVLightSliderTall<WhiteLight>>(mm2px(Vec(204.18, 75.001)), module, _16n::FADER_PARAMS + 15, _16n::FADER_LIGHTS + 15));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.18, 109.355)), module, _16n::CV_OUTPUT + 0));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.18, 109.355)), module, _16n::CV_OUTPUT + 1));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(35.18, 109.355)), module, _16n::CV_OUTPUT + 2));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(48.18, 109.355)), module, _16n::CV_OUTPUT + 3));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(61.18, 109.355)), module, _16n::CV_OUTPUT + 4));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(74.18, 109.355)), module, _16n::CV_OUTPUT + 5));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(87.18, 109.355)), module, _16n::CV_OUTPUT + 6));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(100.18, 109.355)), module, _16n::CV_OUTPUT + 7));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(113.18, 109.355)), module, _16n::CV_OUTPUT + 8));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(126.18, 109.355)), module, _16n::CV_OUTPUT + 9));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(139.18, 109.355)), module, _16n::CV_OUTPUT + 10));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(152.18, 109.355)), module, _16n::CV_OUTPUT + 11));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(165.18, 109.355)), module, _16n::CV_OUTPUT + 12));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(178.18, 109.355)), module, _16n::CV_OUTPUT + 13));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(191.18, 109.355)), module, _16n::CV_OUTPUT + 14));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(204.18, 109.355)), module, _16n::CV_OUTPUT + 15));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.18, 109.355)), module, _16n::CV_OUTPUTS + 0));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(22.18, 109.355)), module, _16n::CV_OUTPUTS + 1));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(35.18, 109.355)), module, _16n::CV_OUTPUTS + 2));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(48.18, 109.355)), module, _16n::CV_OUTPUTS + 3));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(61.18, 109.355)), module, _16n::CV_OUTPUTS + 4));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(74.18, 109.355)), module, _16n::CV_OUTPUTS + 5));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(87.18, 109.355)), module, _16n::CV_OUTPUTS + 6));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(100.18, 109.355)), module, _16n::CV_OUTPUTS + 7));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(113.18, 109.355)), module, _16n::CV_OUTPUTS + 8));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(126.18, 109.355)), module, _16n::CV_OUTPUTS + 9));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(139.18, 109.355)), module, _16n::CV_OUTPUTS + 10));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(152.18, 109.355)), module, _16n::CV_OUTPUTS + 11));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(165.18, 109.355)), module, _16n::CV_OUTPUTS + 12));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(178.18, 109.355)), module, _16n::CV_OUTPUTS + 13));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(191.18, 109.355)), module, _16n::CV_OUTPUTS + 14));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(204.18, 109.355)), module, _16n::CV_OUTPUTS + 15));
 
 		// Todo? replace this with hard-coded? or not?
 		MidiDisplay* display = createWidget<MidiDisplay>(mm2px(Vec(86.360, 13.048)));
